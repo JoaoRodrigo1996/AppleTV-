f4300897d7b1cb7964c4890884a6f924343b08f2
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2024-07-16 14:43:18 -0300

    config: initial configuration react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <>
+      <header className="">Apple TV+</header>
+      <main className="">
+        <div className="">Hero components</div>
+        <div className="">usps</div>
+        <div className="">3 col layouts</div>
+        <div className="">Carousel with posters</div>
+      </main>
+    </>
+  )
+}
